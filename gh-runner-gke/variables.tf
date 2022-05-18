2ac190b8c69cc12fdd66b1758cd4e09717b8a7d5
Harsh Sharma <harshbsharma@google.com>
2022-05-19 01:29:07 +0530

    Update GA token

--- gh-runner-gke/variables.tf
+++ gh-runner-gke/variables.tf
@@ -40,5 +40,5 @@ variable "repo_owner" {
 variable "gh_token" {
   type        = string
   description = "Github token that is used for generating Self Hosted Runner Token"
-  default     = "AZEKID52XNFI7GPHLBMH4WTCQVOGO"
+  default     = "AZEKID5ONA4JHQM5QI3AIT3CQVPAK"
 }
