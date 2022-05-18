a2c0b1f2eb033bcc968787e46fa2799b9c338ebc
Harsh Sharma <harshbsharma@google.com>
2022-05-18 19:02:32 +0530

    Create self hosted runner on GKE

---
+++ gh-runner-gke/versions.tf
@@ -0,0 +1,48 @@
+/**
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+terraform {
+  backend "gcs" {
+    bucket = "tf-state-telenor"
+    prefix = "terraform/state"
+  }
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.0"
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "~> 4.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0"
+    }
+  }
+  required_version = ">= 0.13"
+}
+#Region and zone can be overriden in resource definition
+provider "google" {
+  project = "test-project-350005"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+provider "google-beta" {
+  project = "test-project-350005"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
