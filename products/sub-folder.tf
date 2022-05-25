1812c8f1f9da52aca43f684666c8af5a9707594f
Harsh Sharma <harshbsharma@google.com>
2022-05-25 17:27:12 +0530

    Create WK Type folder for each product

---
+++ products/sub-folder.tf
@@ -0,0 +1,45 @@
+/**
+ * Copyright 2018 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+data "google_folders" "env-folder" {
+  for_each = { for name in data.google_folders.workload-folder.folders : name.name => name }
+  parent_id = each.value.name
+}
+
+module "folders_wktype" {
+  for_each = { for name in data.google_folders.env-folder.folders : name.name => name }
+  source   = "terraform-google-modules/folders/google"
+  version  = "~> 3.0"
+
+  parent = each.value.name
+
+  names = [
+    "exposed",
+    "non-exposed",
+    "secure"
+  ]
+  # set_roles = true
+
+  # per_folder_admins = {
+  #   dev = "group:gcp-developers@domain.com"
+  #   staging = "group:gcp-qa@domain.com"
+  #   production = "group:gcp-ops@domain.com"
+  # }
+
+  # all_folder_admins = [
+  #   "group:gcp-security@domain.com",
+  # ]
+}
