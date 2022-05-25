e40b4f16564150135dc237c1c4af76ba8f827ce3
Harsh Sharma <harshbsharma@google.com>
2022-05-25 19:42:12 +0530

    Add output for products folder

--- products/outputs.tf
+++ products/outputs.tf
@@ -8,12 +8,7 @@ output "ids" {
 #   value       = module.folders_env.names
 # }
 
-# output "ids_list" {
-#   description = "List of folder ids."
-#   value       = module.folders_env.ids_list
-# }
-
-output "names_list" {
-  description = "List of folder names."
-  value       = values(module.folders_env)[*].names_list
+output "ids_list" {
+  description = "List of folder ids."
+  value       = values(module.folders_env)[*].ids_list
 }
