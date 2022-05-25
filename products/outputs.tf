841c9c3a3b207b7c57cc119fb404145396e22ed0
Harsh Sharma <harshbsharma@google.com>
2022-05-25 17:02:55 +0530

    Add outputs file for env

---
+++ products/outputs.tf
@@ -0,0 +1,19 @@
+output "ids" {
+  description = "Folder ids."
+  value       = module.folders_env.ids
+}
+
+output "names" {
+  description = "Folder names."
+  value       = module.folders_env.names
+}
+
+output "ids_list" {
+  description = "List of folder ids."
+  value       = module.folders_env.ids_list
+}
+
+output "names_list" {
+  description = "List of folder names."
+  value       = module.folders_env.names_list
+}
