28e0e043e315dc5c19e98400b612776be3bdeeee
Harsh Sharma <harshbsharma@google.com>
2022-06-03 00:16:02 +0530

    Add var-file to security

---
+++ security/security-terraform.tfvars
@@ -0,0 +1,14 @@
+org_id = "435160234064"
+
+division_foldername = "Security"
+
+environments = ["Dev", "Stage", "Prod"]
+
+billing_account = "01346D-935252-68647E"
+
+#The project-id would be like prj-telenor-${ENV}-${SUFFIX}"
+kms_project_suffix = "kms"
+
+logsinks_project_suffix = "logsink"
+
+scc_project_suffix = "scc"
--- security/variables.tf
+++ security/variables.tf
@@ -17,41 +17,34 @@
 variable "org_id" {
   description = "The organization ID."
   type        = string
-  default     = "435160234064"
 }
 
 variable "division_foldername" {
   description = "The Name of Division folder under which we want to create folder"
   type        = string
-  default     = "Security"
 }
 
 variable "environments" {
   type        = list(string)
   description = "Folder names for each environment."
-  default     = ["Dev", "Stage", "Prod"]
 }
 
 variable "billing_account" {
   description = "The ID of the billing account to associate this project with"
   type        = string
-  default     = "01346D-935252-68647E"
 }
 
 variable "kms_project_suffix" {
   type        = string
   description = "Project Suffix of the KMS project."
-  default     = "kms"
 }
 
 variable "logsinks_project_suffix" {
   type        = string
   description = "Project Suffix of the Log Sink project."
-  default     = "logsink"
 }
 
 variable "scc_project_suffix" {
   type        = string
   description = "Project Suffix of the SCC project."
-  default     = "scc"
 }
