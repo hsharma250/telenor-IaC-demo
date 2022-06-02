5184eaac403db721d57a5b600db05dbae98677bc
Harsh Sharma <harshbsharma@google.com>
2022-06-03 00:19:35 +0530

    Add var-file to platform

---
+++ platform/platform-terraform.tfvars
@@ -0,0 +1,12 @@
+org_id = "435160234064"
+
+division_foldername = "Platform"
+
+environments = ["Dev", "Stage", "Prod"]
+
+billing_account = "01346D-935252-68647E"
+
+#The project-id would be like prj-telenor-${ENV}-${SUFFIX}"
+svpc_project_suffix = "svpc"
+
+gimages_project_suffix = "gimages"
--- platform/variables.tf
+++ platform/variables.tf
@@ -17,35 +17,29 @@
 variable "org_id" {
   description = "The organization ID."
   type        = string
-  default     = "435160234064"
 }
 
 variable "division_foldername" {
   description = "The Name of Division folder under which we want to create folder"
   type        = string
-  default     = "Platform"
 }
 
 variable "environments" {
   type        = list(string)
   description = "Folder names for each environment."
-  default     = ["Dev", "Stage", "Prod"]
 }
 
 variable "billing_account" {
   description = "The ID of the billing account to associate this project with"
   type        = string
-  default     = "01346D-935252-68647E"
 }
 
 variable "svpc_project_suffix" {
   type        = string
   description = "Project ID suffix of the svpc project."
-  default     = "svpc"
 }
 
 variable "gimages_project_suffix" {
   type        = string
   description = "Project ID suffix of the gimages project."
-  default     = "gimages"
 }
